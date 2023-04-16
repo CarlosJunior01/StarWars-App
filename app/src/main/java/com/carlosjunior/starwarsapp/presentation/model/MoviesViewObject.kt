a86b3870f196d05a30f7c6988ac3de56e1e519a8
Carlos Junior <carlos.udi@live.com>
2023-04-16 14:42:53 -0300

    [DEV-APP] Add new models in application
    Added new models MoviesViewObject and PersonsViewObject.

---
+++ app/src/main/java/com/carlosjunior/starwarsapp/presentation/model/MoviesViewObject.kt
@@ -0,0 +1,28 @@
+package com.carlosjunior.starwarsapp.presentation.model
+
+import android.os.Parcelable
+import com.carlosjunior.core.domain.model.Movies
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class MoviesViewObject(
+    val title: String?,
+    val episodeId: Int?,
+    val openingCrawl: String?,
+    val director: String?,
+    val producer: String?,
+    val releaseDate: String?,
+    val url: String?
+) : Parcelable {
+
+    constructor(movies: Movies) : this(
+        title = movies.title,
+        episodeId = movies.episodeId,
+        openingCrawl = movies.openingCrawl,
+        director = movies.director,
+        producer = movies.producer,
+        releaseDate = movies.releaseDate,
+        url = movies.url
+    )
+}
+
---
+++ app/src/main/java/com/carlosjunior/starwarsapp/presentation/model/PersonsViewObject.kt
@@ -0,0 +1,28 @@
+package com.carlosjunior.starwarsapp.presentation.model
+
+import android.os.Parcelable
+import com.carlosjunior.core.domain.model.Persons
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class PersonsViewObject(
+    val name: String?,
+    val height: String?,
+    val hairColor: String?,
+    val skinColor: String?,
+    val birthYear: String?,
+    val gender: String?,
+    val url: String?
+) : Parcelable {
+
+    constructor(person: Persons) : this(
+        name = person.name,
+        height = person.height,
+        hairColor = person.hairColor,
+        skinColor = person.skinColor,
+        birthYear = person.birthYear,
+        gender = person.gender,
+        url = person.url
+    )
+}
+
