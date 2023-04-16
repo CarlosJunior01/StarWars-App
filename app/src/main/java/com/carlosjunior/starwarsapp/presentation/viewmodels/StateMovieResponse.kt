46a01d5c4b631e07213e497ba1802ca32662d657
Carlos Junior <carlos.udi@live.com>
2023-04-16 14:58:50 -0300

    [DEV-APP] Added new StateResponse in application
    Added sealed class StateMovieResponse.
    Added sealed class StateMovieResponse.

--- app/src/main/java/com/carlosjunior/starwarsapp/presentation/viewmodels/StateMovieResponse.kt
+++ app/src/main/java/com/carlosjunior/starwarsapp/presentation/viewmodels/StateMovieResponse.kt
@@ -1,9 +1,9 @@
 package com.carlosjunior.starwarsapp.presentation.viewmodels
 
 import androidx.paging.PagingData
-import com.carlosjunior.core.domain.model.Movies
+import com.carlosjunior.starwarsapp.presentation.model.MoviesViewObject
 
 sealed class StateMovieResponse {
-    data class StateMoviesSuccess(val it: PagingData<Movies>) : StateMovieResponse()
+    data class StateMoviesSuccess(val it: PagingData<MoviesViewObject>) : StateMovieResponse()
     object StateMoviesLoading: StateMovieResponse()
 }
--- app/src/main/java/com/carlosjunior/starwarsapp/presentation/viewmodels/StatePersonsResponse.kt
+++ app/src/main/java/com/carlosjunior/starwarsapp/presentation/viewmodels/StatePersonsResponse.kt
@@ -1,9 +1,13 @@
 package com.carlosjunior.starwarsapp.presentation.viewmodels
 
 import androidx.paging.PagingData
-import com.carlosjunior.core.domain.model.Persons
+import com.carlosjunior.starwarsapp.presentation.model.MoviesViewObject
+import com.carlosjunior.starwarsapp.presentation.model.PersonsViewObject
 
 sealed class StatePersonsResponse {
-    data class StatePersonsSuccess(val it: PagingData<Persons>) : StatePersonsResponse()
+    data class StatePersonsSuccess(val it: PagingData<PersonsViewObject>): StatePersonsResponse()
+    data class StateSearchPersonsSuccess(val personsVO: List<PersonsViewObject>): StatePersonsResponse()
+    data class StateSearchMoviesSuccess(val moviesVO: List<MoviesViewObject>): StatePersonsResponse()
     object StatePersonsLoading: StatePersonsResponse()
+    object StatePersonsError: StatePersonsResponse()
 }
