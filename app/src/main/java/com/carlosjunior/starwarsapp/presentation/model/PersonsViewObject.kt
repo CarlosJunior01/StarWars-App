b586c2c35fa503ab12e5bfe2e5af1006f699eae6
Carlos Junior <carlos.udi@live.com>
2023-04-17 00:53:14 -0300

    [DEV-APP] Update constructor signature overload in PersonsViewObject
    Update constructor to receive the Person model class from the database.

--- app/src/main/java/com/carlosjunior/starwarsapp/presentation/model/PersonsViewObject.kt
+++ app/src/main/java/com/carlosjunior/starwarsapp/presentation/model/PersonsViewObject.kt
@@ -2,6 +2,7 @@ package com.carlosjunior.starwarsapp.presentation.model
 
 import android.os.Parcelable
 import com.carlosjunior.core.domain.model.Persons
+import com.carlosjunior.starwarsapp.database.model.Person
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
@@ -16,6 +17,16 @@ data class PersonsViewObject(
 ) : Parcelable {
 
     constructor(person: Persons) : this(
+        name = person.name,
+        height = person.height,
+        hairColor = person.hairColor,
+        skinColor = person.skinColor,
+        birthYear = person.birthYear,
+        gender = person.gender,
+        url = person.url
+    )
+
+    constructor(person: Person) : this(
         name = person.name,
         height = person.height,
         hairColor = person.hairColor,
